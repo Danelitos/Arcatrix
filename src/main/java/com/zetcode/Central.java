987e1a9d59eef8deca2ac74c51c836e1c25af24a
Mohamed El Basri Dehbi <mohamedelbasri199@gmail.com>
2022-12-04 11:42:04 +0100

    Métodos Central.java

--- src/main/java/com/zetcode/Central.java
+++ src/main/java/com/zetcode/Central.java
@@ -1,4 +1,31 @@
 package com.zetcode;
 
 public class Central {
+    public void iniciarSesion(String nombre, String password){
+
+    }
+
+    public void crearUsuario(String nombre, String correo, String password, String repetirPassword){
+
+    }
+
+    public void recuperarContrasena(String correo){
+
+    }
+
+    public void actualizarDatos(){
+
+    }
+
+    public void guardarPartida(int codUsuario, Partida laPartida){
+
+    }
+
+    public void cargarPartida(int codUsuario, int codPartida, String fechaHora){
+
+    }
+
+    public boolean obtPersonalizacion(int codUsu){
+        return false;
+    }
 }
