50f4fdfef89faa0978a768c53f0968b833ff0aff
Mohamed El Basri Dehbi <mohamedelbasri199@gmail.com>
2022-12-15 21:52:37 +0100

    Requisitos de registro y recuperación de contraseña

--- src/main/java/vista/VentanaRecuperarContrasena.java
+++ src/main/java/vista/VentanaRecuperarContrasena.java
@@ -10,6 +10,10 @@ public class VentanaRecuperarContrasena extends JFrame {
     private JLabel correoText;
     private JTextField correo;
     private JButton recuperarButton;
+    private JComboBox<String> preguntaSeguridad;
+    private JTextField respuestaSeguridad;
+    private JLabel preguntaSeguridadText;
+    private JLabel respuestaSeguridadText;
 
 
     private VentanaRecuperarContrasena() {
@@ -25,14 +29,49 @@ public class VentanaRecuperarContrasena extends JFrame {
 
         setLocationRelativeTo(null);
 
-        recuperarButton = getBoton("RECUPERAR CONTRASEÑA");
-        correo = new JTextField();
+        recuperarButton = new JButton("Recuperar");
+        recuperarButton.setBounds(140, 410, 200, 50);
+        recuperarButton.setBackground(new Color(51, 159, 221));
+        recuperarButton.setHorizontalAlignment(SwingConstants.CENTER);
+
+
         correoText = new JLabel("Correo electrónico");
+        correoText.setBounds(100, 20, 150, 20);
+        correoText.setFont(new Font(null, Font.BOLD, 15));
+        correoText.setHorizontalAlignment(SwingConstants.CENTER);
+
+        correo = new JTextField();
+        correo.setBounds(139, 45, 200, 30);
+
+        preguntaSeguridadText = new JLabel("Pregunta de seguridad");
+        preguntaSeguridadText.setBounds(125, 260, 200, 20);
+        preguntaSeguridadText.setFont(new Font(null, Font.BOLD, 15));
+        preguntaSeguridadText.setHorizontalAlignment(SwingConstants.CENTER);
+
+        preguntaSeguridad = new JComboBox<>();
+        preguntaSeguridad.addItem("Nombre de tu primera mascota");
+        preguntaSeguridad.addItem("Nombre de tu primer colegio");
+        preguntaSeguridad.addItem("Nombre de tu mejor amigo/a");
+        preguntaSeguridad.addItem("Nombre de tu primo/a favorito/a");
+        preguntaSeguridad.setBounds(139, 285, 200, 30);
+
+        respuestaSeguridadText = new JLabel("Respuesta de seguridad");
+        respuestaSeguridadText.setBounds(129, 320, 200, 20);
+        respuestaSeguridadText.setFont(new Font(null, Font.BOLD, 15));
+        respuestaSeguridadText.setHorizontalAlignment(SwingConstants.CENTER);
+
+        respuestaSeguridad = new JTextField();
+        respuestaSeguridad.setBounds(139, 345, 200, 30);
 
 
-        panelRecuperar.add(correoText, BorderLayout.CENTER);
-        panelRecuperar.add(correo, BorderLayout.CENTER);
-        panelRecuperar.add(recuperarButton, BorderLayout.CENTER);
+
+        panelRecuperar.add(correoText);
+        panelRecuperar.add(correo);
+        panelRecuperar.add(preguntaSeguridadText);
+        panelRecuperar.add(preguntaSeguridad);
+        panelRecuperar.add(respuestaSeguridadText);
+        panelRecuperar.add(respuestaSeguridad);
+        panelRecuperar.add(recuperarButton);
 
         setVisible(true);
     }
--- src/main/java/vista/VentanaRegistro.java
+++ src/main/java/vista/VentanaRegistro.java
@@ -1,6 +1,8 @@
 package vista;
 
+import com.zetcode.Central;
 import com.zetcode.GestorBD;
+import com.zetcode.GestorUsuarios;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,25 +17,34 @@ public class VentanaRegistro extends JFrame {
     private JLabel correoText;
     private JLabel passwordText;
     private JLabel repetirPasswordText;
+    private JLabel preguntaSeguridadText;
+    private JLabel respuestaSeguridadText;
+    private JLabel avisoUsuario;
+    private JLabel avisoCorreo;
+    private JLabel avisoPassword;
+    private JLabel avisoRepetirPassword;
 
     private JTextField usuario;
     private JTextField correo;
     private JPasswordField password;
     private JPasswordField repetirPassword;
-
+    private JComboBox<String> preguntaSeguridad;
+    private JTextField respuestaSeguridad;
     private JButton registrarse;
     private JButton volver;
 
     private VentanaRegistro() {
         super("ARCATRIX - REGISTRO");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(500, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);  //darle a la X, se acaba el proceso
         setLocationRelativeTo(null); //en el centro de la pantalla
 
         //inicar componentes
         this.setVisible(true);
         setComponentes();
+
+        validar();
     }
 
     public void setComponentes() {
@@ -69,9 +80,30 @@ public class VentanaRegistro extends JFrame {
         repetirPasswordText.setFont(new Font(null, Font.BOLD, 15));
         repetirPasswordText.setHorizontalAlignment(SwingConstants.CENTER);
 
+        //parte para recuperar contraseña
+        preguntaSeguridadText = new JLabel("Pregunta de seguridad");
+        preguntaSeguridadText.setBounds(125, 260, 200, 20);
+        preguntaSeguridadText.setFont(new Font(null, Font.BOLD, 15));
+        preguntaSeguridadText.setHorizontalAlignment(SwingConstants.CENTER);
+
+        preguntaSeguridad = new JComboBox<>();
+        preguntaSeguridad.addItem("Nombre de tu primera mascota");
+        preguntaSeguridad.addItem("Nombre de tu primer colegio");
+        preguntaSeguridad.addItem("Nombre de tu mejor amigo/a");
+        preguntaSeguridad.addItem("Nombre de tu primo/a favorito/a");
+        preguntaSeguridad.setBounds(139, 285, 200, 30);
+
+        respuestaSeguridadText = new JLabel("Respuesta de seguridad");
+        respuestaSeguridadText.setBounds(129, 320, 200, 20);
+        respuestaSeguridadText.setFont(new Font(null, Font.BOLD, 15));
+        respuestaSeguridadText.setHorizontalAlignment(SwingConstants.CENTER);
+
+        respuestaSeguridad = new JTextField();
+        respuestaSeguridad.setBounds(139, 345, 200, 30);
+
         //BOTONES
         registrarse = new JButton("Crear cuenta");
-        registrarse.setBounds(140, 300, 200, 50);
+        registrarse.setBounds(140, 410, 200, 50);
         registrarse.setBackground(new Color(51, 159, 221));
         registrarse.setHorizontalAlignment(SwingConstants.CENTER);
         registrarse.addActionListener(evento -> {
@@ -82,10 +114,35 @@ public class VentanaRegistro extends JFrame {
             }
         });
         volver = new JButton("Volver");
-        volver.setBounds(140, 380, 200, 50);
+        volver.setBounds(140, 490, 200, 50);
         volver.setBackground(new Color(51, 159, 221));
         volver.setHorizontalAlignment(SwingConstants.CENTER);
         volver.addActionListener(evento -> volver());
+
+        //validar
+        avisoUsuario = new JLabel();
+        avisoUsuario.setBounds(290, 45, 200, 30);
+        avisoUsuario.setFont(new Font(null, Font.BOLD, 9));
+        avisoUsuario.setForeground(Color.red);
+        avisoUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+
+        avisoCorreo = new JLabel();
+        avisoCorreo.setBounds(290, 105, 200, 30);
+        avisoCorreo.setFont(new Font(null, Font.BOLD, 9));
+        avisoCorreo.setForeground(Color.red);
+        avisoCorreo.setHorizontalAlignment(SwingConstants.CENTER);
+
+        avisoPassword = new JLabel();
+        avisoPassword.setBounds(290, 165, 200, 30);
+        avisoPassword.setFont(new Font(null, Font.BOLD, 9));
+        avisoPassword.setForeground(Color.red);
+        avisoPassword.setHorizontalAlignment(SwingConstants.CENTER);
+
+        avisoRepetirPassword = new JLabel();
+        avisoRepetirPassword.setBounds(290, 225, 200, 30);
+        avisoRepetirPassword.setFont(new Font(null, Font.BOLD, 9));
+        avisoRepetirPassword.setForeground(Color.red);
+        avisoRepetirPassword.setHorizontalAlignment(SwingConstants.CENTER);
 
         panelRegistro.add(usuarioText);
         panelRegistro.add(usuario);
@@ -97,6 +154,19 @@ public class VentanaRegistro extends JFrame {
         panelRegistro.add(repetirPassword);
         panelRegistro.add(registrarse);
         panelRegistro.add(volver);
+
+        panelRegistro.add(avisoUsuario);
+        panelRegistro.add(avisoCorreo);
+        panelRegistro.add(avisoPassword);
+        panelRegistro.add(avisoRepetirPassword);
+
+        panelRegistro.add(preguntaSeguridadText);
+        panelRegistro.add(preguntaSeguridad);
+        panelRegistro.add(respuestaSeguridadText);
+        panelRegistro.add(respuestaSeguridad);
+
+
+
     }
 
     public static VentanaRegistro getInstance() {
@@ -129,6 +199,44 @@ public class VentanaRegistro extends JFrame {
     public void volver() {
         VentanaRegistro.getInstance().setVisible(false);
         VentanaLogin.getInstance().setVisible(true);
+    }
+
+    public void validar(){
+        if(usuarioText.getText().isEmpty()){
+            avisoUsuario.setText("*campo requerido");
+        }else{
+            avisoUsuario.setText("");
+        }
+
+        if(correoText.getText().isEmpty()){
+            avisoCorreo.setText("*campo requerido");
+        }else if(!correoText.getText().contains("@") || !correoText.getText().contains(".")){
+            avisoCorreo.setText("¡no es válido!");
+        }else{
+            avisoCorreo.setText("");
+        }
+
+        if(passwordText.getText().isEmpty()){
+            avisoPassword.setText("*campo requerido");
+        }else{
+            avisoPassword.setText("");
+        }
+
+        if(repetirPasswordText.getText().isEmpty()){
+            avisoRepetirPassword.setText("*campo requerido");
+        }else{
+            avisoRepetirPassword.setText("");
+        }
+
+        if(usuarioText.getText().isEmpty() || correoText.getText().isEmpty() || passwordText.getText().isEmpty() || repetirPasswordText.getText().isEmpty()){
+            registrarse.setEnabled(false);
+        }else{
+            registrarse.setEnabled(true);
+        }
+    }
+
+    private void usuarioTextKeyReleased(java.awt.event.KeyEvent evt){
+        validar();
     }
 
 }
